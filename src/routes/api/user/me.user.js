2ab23642e72964724622e6b0c872f8a015a5696a
Gleb Mikheenko <glebmikheenk0o@gmail.com>
2020-01-08 21:17:59 +0300

    fix sms generateToken issue

--- src/routes/api/user/me.user.js
+++ src/routes/api/user/me.user.js
@@ -1,3 +1,3 @@
 module.exports = (req, res) => {
-  res.success(req.user);
+  res.success({ user: req.user });
 };
--- src/routes/api/user/update_me.user.js
+++ src/routes/api/user/update_me.user.js
@@ -2,9 +2,9 @@ const { ProfileService } = require('../../../services');
 
 module.exports = async (req, res) => {
   try {
-    const updatedUser = await ProfileService.updateProfile({ ...req.body, id: req.user._id });
+    const user = await ProfileService.updateProfile({ ...req.body, id: req.user._id });
     res.success({
-      updatedUser
+      user
     });
   } catch(err) {
     res.error({
