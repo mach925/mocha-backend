d478d4de62148b36fa48fac54a6cbad35526e25c
wizardev2019 <chedo.tdev@gmail.com>
2020-01-17 19:56:11 +0200

    fix routing for getting all own trustnetwork

--- src/routes/api/network/index.js
+++ src/routes/api/network/index.js
@@ -6,6 +6,6 @@ route.post('/', require('./create.network'));
 route.get('/:id', require('./get.network'));
 route.patch('/:id', require('./update.network'));
 route.delete('/:id', require('./remove.network'));
-route.get('/list', require('./list.network'));
+route.get('/', require('./list.network'));
 
 module.exports = route;
